784a83b75cfe281fd81d1847e5294531df0ac54e
Ethan <kingetha1@esc.vuw.ac.nz>
2020-06-17 14:45:34 +1200

    Added the project structure for the CSV parser
    To begin with the CSV parser will kept apart from the other pieces of code we are currently working on.The aim for this first interation of the CSV parser is that it's going to take the export from open rocket simulation, parse the information and then display in to the console.
    
    Copied from the the CSV parser readme
    
    author: Ethan King

---
+++ architecture_design/Code/Parsers/CSV/parser/CSV_PARSER.java
@@ -0,0 +1,12 @@
+package Parsers.CSV.parser;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class CSV_PARSER{
+    public static void main(String[] args) {
+        System.out.println("Testing project structure");
+    }
+}
