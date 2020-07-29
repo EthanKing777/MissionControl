61c4ddf4f9b96007557621368f1d0142d476be3e
Thomas Rainford <thomas@rainfords.net>
2020-07-30 09:17:08 +1200

    Removed opens and exports from module-info.java.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,8 +3,6 @@ module org.group11 {
     requires javafx.fxml;
 
     opens org.group11 to javafx.fxml;
-    opens org.group11.controller.component to javafx.fxml;
 
     exports org.group11;
-    exports org.group11.controller.component;
 }
