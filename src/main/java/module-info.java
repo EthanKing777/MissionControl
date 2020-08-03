a13921ddd3ee81c644e5fb5e5ec645ca442441c7
Thomas Rainford <thomas@rainfords.net>
2020-08-03 12:24:47 +1200

    Add weather model class for hourly weather. Add controller for weather tab. Add JUnit dependency.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,8 @@ module org.group11 {
     requires javafx.fxml;
 
     opens org.group11 to javafx.fxml;
+    opens org.group11.controller to javafx.fxml;
 
     exports org.group11;
+    exports org.group11.controller;
 }
---
+++ src/main/java/org/group11/controller/WeatherTabController.java
@@ -0,0 +1,9 @@
+package org.group11.controller;
+
+public class WeatherTabController {
+
+    public WeatherTabController() {
+    }
+
+
+}
