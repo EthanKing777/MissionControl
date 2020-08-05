c16e693223ff9da1286bf2576a28bdbcd567b0cb
Ruvindu Wijeratne <ruvindu.w@gmail.com>
2020-08-05 23:01:10 +1200

    CSV parser for OpenRocket data Parses data obtained from OpenRocket in .csv format

---
+++ src/main/java/org/group11/SimulationDataParser.java
@@ -0,0 +1,65 @@
+package org.group11;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class SimulationDataParser {
+
+	private String[] headers;
+	private List <String> totalDataSet;
+	
+	public SimulationDataParser() {
+		try {
+			File file = new File ("/Users/ruvindu/Desktop/test_1.csv");
+			Scanner scan = new Scanner (file);
+			headers = new String[14]; //There are 14 headers in the test_1.csv file
+			totalDataSet = new ArrayList <String> ();
+			this.extractData(scan);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void extractData(Scanner scan) {	
+		int i=0;
+		while(scan.hasNext()) {
+			if(scan.hasNext("#")) {
+				scan.next();//Skip the # token
+				headers[i] = scan.nextLine();
+				i++;
+			}
+			else {
+				totalDataSet.add(scan.nextLine());
+			}
+		}
+	}
+	
+	public String[] getHeaders() {
+		return this.headers; 
+	}
+	
+	public List<Number> getVariableData (String type){
+		String[] variables;
+		List<Number> selectedVariableData = new ArrayList <Number> ();
+		int index = -1;
+		
+		if(type.equalsIgnoreCase("time")) {
+			index = 0;
+		}else if(type.equalsIgnoreCase("total velocity")) {
+			index = 4;
+		}else if(type.equalsIgnoreCase("total acceleration")) {
+			index = 5;
+		}
+		
+		for(int i=0; i<totalDataSet.size(); i++) {
+			variables = totalDataSet.get(i).split(",");
+			selectedVariableData.add(Double.parseDouble(variables[index]));
+		} 
+		
+		return selectedVariableData;
+	}
+}
+
