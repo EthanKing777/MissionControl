2e7b05afb93f98b842698a5b9a1c10af2fdc90db
Ruvindu Wijeratne <ruvindu.w@gmail.com>
2020-08-07 01:09:29 +1200

    Links Created Between Tabs and their Controllers - Each tab is linked with its own controller via a button - Except for the "Populate Graph" button in the "Simulation Data" tab, all others print a test message to the console. - This is only to show the links between the components and their controllers, the test buttons have no real functionality

--- src/main/java/org/group11/controller/PreLaunchScreenController.java
+++ src/main/java/org/group11/controller/PreLaunchScreenController.java
@@ -1,9 +1,20 @@
 package org.group11.controller;
 
+import javafx.fxml.FXML;
+
 public class PreLaunchScreenController {
 
 	public PreLaunchScreenController() {
 		// TODO Auto-generated constructor stub
 	}
 
+	@FXML
+	public void printArmButtonOutput() {
+		System.out.println("\n PreLaunchScreenController - ARM Button Pressed \n");
+	}
+	
+	@FXML
+	public void printGoNoGoButtonOutput() {
+		System.out.println("\n PreLaunchScreenController - Go/No-Go Button Pressed \n");
+	}
 }
