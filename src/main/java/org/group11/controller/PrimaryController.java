7e708dc8c2b5e6667d98f97ad19e835d1fb52b68
Ruvindu Wijeratne <ruvindu.w@gmail.com>
2020-08-07 00:26:19 +1200

    Moving @rainfothom 's commit cbe8fe06 to a new class Thomas's weather tab controller code moved from PrimaryController to new WeatherTabController

--- src/main/java/org/group11/controller/PrimaryController.java
+++ src/main/java/org/group11/controller/PrimaryController.java
@@ -8,55 +8,10 @@ import org.group11.model.weather.WeatherData;
 import java.io.*;
 
 
-/**
- * Controller for the weather tab. This controller will
- * have access to a weather api to fetch weather data and control
- * the GUI inside the weather tab.
- */
 public class PrimaryController {
-
-    /* Weather Tab Parameters */
-
-    // Gives access to the weather data.
-    private WeatherData weatherData;
-
-
-
-    /*  Tab Parameters */
-
-
-    /*  Tab Parameters */
 
     public PrimaryController() {
     }
 
-    /* ==== Weather Tab Methods ==== */
-    @FXML
-    public void getWeatherData() {
-        // NOTE: The lat and lon values will be obtained from a form.
-        // Fetches and parses the weather data.
-        WeatherDataFetcherParser wdfp = new WeatherDataFetcherParser(41.0, -41.0);
-
-        try {
-            weatherData = wdfp.fetchWeatherData();
-        } catch (IOException e) {
-
-        }
-        System.out.println("DONE");
-    }
-
-    @FXML
-    public void displayWeatherData() {
-        for (HourlyWeather hourlyWeather : weatherData.getHourlyData()) {
-            System.out.println(hourlyWeather.toString());
-        }
-    }
-
-
-
-    /* ====  Tab Methods ==== */
-
-
-    /* ====  Tab Methods ==== */
 }
 
--- src/main/java/org/group11/controller/WeatherTabController.java
+++ src/main/java/org/group11/controller/WeatherTabController.java
@@ -1,9 +1,51 @@
 package org.group11.controller;
 
-public class WeatherTabController {
+import java.io.IOException;
 
+import org.group11.controller.weather.WeatherDataFetcherParser;
+import org.group11.model.weather.HourlyWeather;
+import org.group11.model.weather.WeatherData;
+
+import javafx.fxml.FXML;
+
+/**
+ * Controller for the weather tab. This controller will
+ * have access to a weather api to fetch weather data and control
+ * the GUI inside the weather tab.
+ */
+public class WeatherTabController {
+	
+    /* Weather Tab Parameters */
+
+    // Gives access to the weather data.
+    private WeatherData weatherData;
+
+    
 	public WeatherTabController() {
-		// TODO Auto-generated constructor stub
+		getWeatherData();
 	}
+	
+	/* ==== Weather Tab Methods ==== */
+    @FXML
+    public void getWeatherData() {
+        // NOTE: The lat and lon values will be obtained from a form.
+        // Fetches and parses the weather data.
+        WeatherDataFetcherParser wdfp = new WeatherDataFetcherParser(41.0, -41.0);
+
+        try {
+            weatherData = wdfp.fetchWeatherData();
+        } catch (IOException e) {
+
+        }
+    }
+
+    @FXML
+    public void displayWeatherData() {
+    	System.out.println("*********  Start of WeatherTabController Output  ********* \n");
+        for (HourlyWeather hourlyWeather : weatherData.getHourlyData()) {
+            System.out.println(hourlyWeather.toString());
+        }
+        System.out.println("\n *********  End of WeatherTabController Output   **********");
+    }
 
 }
