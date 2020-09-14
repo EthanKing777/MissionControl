79c09e3b6a61a2db85fc79a94223e4e9e0d6b0dc
Nirari Awas <awasnira@ecs.vuw.ac.nz>
2020-09-15 02:02:36 +1200

    Made the edit simulation table (create/edit simulation controller) editable and added methods to populate the table.

--- src/main/java/org/group11/controller/CreateEditSimulationController.java
+++ src/main/java/org/group11/controller/CreateEditSimulationController.java
@@ -17,25 +17,44 @@ public class CreateEditSimulationController implements Initializable {
 	private TableView<PropertyTableModel> editWeatherTable;
 
 	@FXML
+	private TableView<PropertyTableModel> editSimulationTable;
+
+	@FXML
 	private TableColumn<PropertyTableModel, String> weatherIdCol;
 
 	@FXML
 	private TableColumn<PropertyTableModel, String> weatherValueCol;
 
+	@FXML
+	private TableColumn<PropertyTableModel, String> simulationIdCol;
+
+	@FXML
+	private TableColumn<PropertyTableModel, String> simulationValueCol;
+
 	private ObservableList<PropertyTableModel> weatherTableProperties = FXCollections.observableArrayList();
+	private ObservableList<PropertyTableModel> simulationTableProperties = FXCollections.observableArrayList();
+
 
 	@Override
 	public void initialize(URL url, ResourceBundle resourceBundle) {
+
+		//Weather table columns
 		weatherIdCol.setCellValueFactory(cellData -> cellData.getValue().getPropertyId());
 		weatherValueCol.setCellValueFactory(cellData -> cellData.getValue().getPropertyValue());
 		weatherValueCol.setCellFactory(TextFieldTableCell.forTableColumn()); //Make the value column editable
 
+		//Simulation table columns
+		simulationIdCol.setCellValueFactory(cellData -> cellData.getValue().getPropertyId());
+		simulationValueCol.setCellValueFactory(cellData -> cellData.getValue().getPropertyValue());
+		simulationValueCol.setCellFactory(TextFieldTableCell.forTableColumn()); //Make the value column editable
+
+		//Update the rows stored in the tables
 		editWeatherTable.setItems(weatherTableProperties);
+		editSimulationTable.setItems(simulationTableProperties);
 	}
 
 	@FXML
 	public void printSubmitButtonOutput() {
 		System.out.println("\n CreateEditSimulationController - Submit Button Pressed \n");
-		weatherTableProperties.add(new PropertyTableModel("R", Math.random() + ""));
 	}
 }
