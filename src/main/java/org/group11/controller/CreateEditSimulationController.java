7606680cc712911ca024a21a8bfb9d8aab9e7e76
Nirari Awas <awasnira@ecs.vuw.ac.nz>
2020-09-15 01:37:26 +1200

    Making the value column on the weather data editable (create/edit simulation controller), so the properties can be edited on the user interface.

--- src/main/java/org/group11/controller/CreateEditSimulationController.java
+++ src/main/java/org/group11/controller/CreateEditSimulationController.java
@@ -1,4 +1,4 @@
-package org.group11.controller;
+package org.group11.controller.fxcontrollers;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -6,6 +6,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.TextFieldTableCell;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -27,6 +28,7 @@ public class CreateEditSimulationController implements Initializable {
 	public void initialize(URL url, ResourceBundle resourceBundle) {
 		weatherIdCol.setCellValueFactory(cellData -> cellData.getValue().getPropertyId());
 		weatherValueCol.setCellValueFactory(cellData -> cellData.getValue().getPropertyValue());
+		weatherValueCol.setCellFactory(TextFieldTableCell.forTableColumn()); //Make the value column editable
 
 		editWeatherTable.setItems(weatherTableProperties);
 	}
