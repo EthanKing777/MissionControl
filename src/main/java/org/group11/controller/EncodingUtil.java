7da5e111f3a157a447d542a0a63faf27f7a7baa0
Ruvindu Wijeratne <ruvindu.w@gmail.com>
2020-10-07 02:11:02 +1300

    URL encoder util class added. Sourced from stackoverflow.com - This class can be used to encode the URL in the exact format used by the encodeURIComponent method in JavaScript

--- src/main/java/org/group11/controller/EncodingUtil.java
+++ src/main/java/org/group11/controller/EncodingUtil.java
@@ -9,6 +9,7 @@ import java.net.URLEncoder;
  * 
  * @see http://stackoverflow.com/questions/607176/java-equivalent-to-javascripts-encodeuricomponent-that-produces-identical-output
  * @author John Topley 
+ * 
  */
 public class EncodingUtil
 {
